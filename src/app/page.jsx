5c15feb6e2a617c44ca9ac267925e6ab461777d7
Matthew Traul <mclydep@gmail.com>
2023-02-04 23:49:53 -0600

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.center}>
       <h1 className={`base-input-class ${inter.className} ${styles.h1}`}>
-        BeyondSite is evolving soon, different, and better than ever.
+        BeyondSite is evolving soon. It will be different, and better than ever.
       </h1>
       <p className={`base-input-class ${inter.className} ${styles.description}`}>
         We’re in the process of building a new website while we roll out an entirely new brand identity system. This experiential website will show a complete picture of the event services we provide, the principles that guide us, the types of work we’re best at, and unique pathways for clients to connect with & hire us for event program management, staffing, and event staff leadership; event workers to join the tribe & work with us; and the global events community to see what we’re up to.
