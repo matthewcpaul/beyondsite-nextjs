a9e11aa2e7f275609f14268b35c0412dc07e812b
Matthew Traul <mclydep@gmail.com>
2023-02-06 17:08:09 -0600

    Smart apostrophe

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,7 +12,7 @@ export default function Home() {
         Welcome to BeyondSite, your event staffing leadership partner for big-production events and conferences.
         </h1>
         <p className={`base-input-class ${inter.className} ${styles.description}`}>
-          Our website is currently undergoing a relaunch with a brand new look and feel, and we're stoked to share our story and services in an experiential fashion. Check back in March for our official brand launch. For any current event staffing and management inquiries, please feel free to reach out to us at <a href="mailto:ryan@beyond.site">ryan@beyond.site</a>.
+          Our website is currently undergoing a relaunch with a brand new look and feel, and we’re stoked to share our story and services in an experiential fashion. Check back in March for our official brand launch. For any current event staffing and management inquiries, please feel free to reach out to us at <a href="mailto:ryan@beyond.site">ryan@beyond.site</a>.
         </p>
         <p className={`base-input-class ${inter.className} ${styles.description}`}>
         See you on-site in the meantime!
